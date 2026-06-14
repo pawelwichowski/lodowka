16ad8bdd91472bf1ac71beab61e2a58a4b361de7
pawelwichowski <161590855+pawelwichowski@users.noreply.github.com>
2026-06-14 20:00:04 +0000

    Fix: Specify type parameters explicitly for Moshi adapter

--- app/src/main/java/com/reypablo/lodowka/data/local/database/Converters.kt
+++ app/src/main/java/com/reypablo/lodowka/data/local/database/Converters.kt
@@ -5,41 +5,50 @@ import com.reypablo.lodowka.domain.models.RecipeIngredient
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import java.lang.reflect.ParameterizedType
 
 class Converters {
     private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
         .build()
     
-    private val recipeIngredientListType = Types.newParameterizedType(
-        List::class.java, 
-        RecipeIngredient::class.java
-    )
-    private val recipeIngredientListAdapter = moshi.adapter(recipeIngredientListType)
-    
-    private val stringListType = Types.newParameterizedType(
-        List::class.java, 
-        String::class.java
-    )
-    private val stringListAdapter = moshi.adapter(stringListType)
-
     @TypeConverter
     fun fromRecipeIngredientList(ingredients: List<RecipeIngredient>?): String? {
-        return ingredients?.let { recipeIngredientListAdapter.toJson(it) }
+        val type: ParameterizedType = Types.newParameterizedType(
+            List::class.java,
+            RecipeIngredient::class.java
+        )
+        val adapter = moshi.adapter<List<RecipeIngredient>>(type)
+        return ingredients?.let { adapter.toJson(it) }
     }
 
     @TypeConverter
     fun toRecipeIngredientList(json: String?): List<RecipeIngredient>? {
-        return json?.let { recipeIngredientListAdapter.fromJson(it) }
+        val type: ParameterizedType = Types.newParameterizedType(
+            List::class.java,
+            RecipeIngredient::class.java
+        )
+        val adapter = moshi.adapter<List<RecipeIngredient>>(type)
+        return json?.let { adapter.fromJson(it) }
     }
 
     @TypeConverter
     fun fromStringList(list: List<String>?): String? {
-        return list?.let { stringListAdapter.toJson(it) }
+        val type: ParameterizedType = Types.newParameterizedType(
+            List::class.java,
+            String::class.java
+        )
+        val adapter = moshi.adapter<List<String>>(type)
+        return list?.let { adapter.toJson(it) }
     }
 
     @TypeConverter
     fun toStringList(json: String?): List<String>? {
-        return json?.let { stringListAdapter.fromJson(it) }
+        val type: ParameterizedType = Types.newParameterizedType(
+            List::class.java,
+            String::class.java
+        )
+        val adapter = moshi.adapter<List<String>>(type)
+        return json?.let { adapter.fromJson(it) }
     }
 }
