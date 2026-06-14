075a06bb2c29c1ce14204e10dd5a6ca3e8642ae3
pawelwichowski <161590855+pawelwichowski@users.noreply.github.com>
2026-06-14 20:09:47 +0000

    Add: ShoppingListViewModel.kt

---
+++ app/src/main/java/com/reypablo/lodowka/presentation/screens/shopping_list/ShoppingListViewModel.kt
@@ -0,0 +1,70 @@
+package com.reypablo.lodowka.presentation.screens.shopping_list
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.reypablo.lodowka.domain.models.ShoppingItem
+import com.reypablo.lodowka.domain.repositories.ShoppingRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class ShoppingListViewModel @Inject constructor(
+    private val shoppingRepository: ShoppingRepository
+) : ViewModel() {
+
+    private val _state = MutableStateFlow<ShoppingListState>(ShoppingListState.Loading)
+    val state: StateFlow<ShoppingListState> = _state.asStateFlow()
+
+    init {
+        loadShoppingList()
+    }
+
+    fun onEvent(event: ShoppingListEvent) {
+        when (event) {
+            ShoppingListEvent.LoadShoppingList -> loadShoppingList()
+            is ShoppingListEvent.AddItem -> addItem(event.item)
+            is ShoppingListEvent.DeleteItem -> deleteItem(event.item)
+            is ShoppingListEvent.ToggleChecked -> toggleChecked(event.item)
+            ShoppingListEvent.ClearAll -> clearAll()
+        }
+    }
+
+    private fun loadShoppingList() {
+        _state.value = ShoppingListState.Loading
+        shoppingRepository.getAllItems()
+            .onEach { items ->
+                _state.value = ShoppingListState.Success(items)
+            }
+            .launchIn(viewModelScope)
+    }
+
+    private fun addItem(item: ShoppingItem) {
+        viewModelScope.launch {
+            shoppingRepository.addItem(item)
+        }
+    }
+
+    private fun deleteItem(item: ShoppingItem) {
+        viewModelScope.launch {
+            shoppingRepository.deleteItem(item)
+        }
+    }
+
+    private fun toggleChecked(item: ShoppingItem) {
+        viewModelScope.launch {
+            val toggled = item.copy(isChecked = !item.isChecked)
+            shoppingRepository.updateItem(toggled)
+        }
+    }
+
+    private fun clearAll() {
+        viewModelScope.launch {
+            shoppingRepository.clearAll()
+        }
+    }
+}
