99020e9cafbc4fb7fec7541fd4cf4a9c2d77e1f3
pawelwichowski <161590855+pawelwichowski@users.noreply.github.com>
2026-06-14 15:14:49 +0000

    Add MainActivity.kt

---
+++ app/src/main/java/com/reypablo/lodowka/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.reypablo.lodowka
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.reypablo.lodowka.presentation.navigation.AppNavHost
+import com.reypablo.lodowka.presentation.theme.LodowkaTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LodowkaTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    AppNavHost()
+                }
+            }
+        }
+    }
+}
