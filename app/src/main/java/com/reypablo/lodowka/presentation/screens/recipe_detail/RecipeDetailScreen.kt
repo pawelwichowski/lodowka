9ccfecf54361cc8c6841e5dc244e664885076bf1
pawelwichowski <161590855+pawelwichowski@users.noreply.github.com>
2026-06-14 16:16:14 +0000

    Add minimal RecipeDetailScreen.kt

---
+++ app/src/main/java/com/reypablo/lodowka/presentation/screens/recipe_detail/RecipeDetailScreen.kt
@@ -0,0 +1,9 @@
+package com.reypablo.lodowka.presentation.screens.recipe_detail
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RecipeDetailScreen() {
+    Text("Recipe Detail Screen")
+}
